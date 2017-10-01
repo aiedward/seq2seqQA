aa61c3a6e22ec43c7e891dd39a18b4fad9ec68c3
thetweak <tweakon@gmail.com>
2017-10-01 12:02:59 +0300

    add recall@k metric

--- dual_encoder.py
+++ dual_encoder.py
@@ -3,11 +3,14 @@ import numpy as np
 import data_utils
 from tensorflow.contrib import layers
 from tqdm import *
+import datetime
 
 
 CONTEXT_ENCODER_UNITS = 50
 RESPONSE_ENCODER_UNITS = 50
 EMBEDDING_SIZE = 300
+TOP_K = 2
+CURRENT_DATE_TIME = datetime.datetime.now().strftime('%d-%m-%Y-%H-%M-%S')
 
 
 tf.app.flags.DEFINE_boolean('train_mode', True, 'Run in a training mode')
@@ -23,6 +26,16 @@ sess = tf.Session()
 questions, q_seq_length, answers, a_seq_length, labels = data_utils.get_data(FLAGS.questions_train,
                                                                              FLAGS.answers_train,
                                                                              FLAGS.labels)
+_, _, answers_all, a_seq_length_all, _ = data_utils.get_data('questions_tokenized_emb.txt',
+                                                                                   'all_answers_tokenized_emb.txt',
+                                                                                   FLAGS.labels)
+questions_eval = {}
+num_distractions = 10
+for i in range(num_distractions):
+    qs = np.array([questions[i] for _ in range(num_distractions)])
+    qs_seq_length = np.array([q_seq_length[i] for _ in range(num_distractions)])
+    questions_eval[i] = qs, qs_seq_length
+
 embeddings_array = np.load('/home/thetweak/Developer/agora_qa_seq2seq/embeddings.npy')
 
 context_encoder_inputs = tf.placeholder(tf.int32, [None, None], name='context_encoder_inputs')
@@ -61,10 +74,14 @@ with tf.variable_scope('learn_matrix'):
 with tf.variable_scope('bias'):
     b = tf.get_variable('b', initializer=tf.constant(0.1, shape=[CONTEXT_ENCODER_UNITS]))
 
+num_examples = tf.get_variable('num_examples', shape=[], dtype=tf.int32)
+num_correct = tf.get_variable('num_correct', shape=[], dtype=tf.int32)
+
 generated_context = response_encoder_state.h @ M + b
 dot_product = tf.squeeze(tf.reduce_sum(tf.multiply(context_encoder_state.h, generated_context), 1, keep_dims=True))
 predictions = tf.sigmoid(dot_product)
-x_entropy = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(labels=tf.to_float(labels_input), logits=dot_product))
+x_entropy_op = tf.nn.sigmoid_cross_entropy_with_logits(labels=tf.to_float(labels_input), logits=dot_product)
+x_entropy = tf.reduce_mean(x_entropy_op)
 train_op = tf.train.AdamOptimizer(1e-4).minimize(x_entropy)
 
 init_op = tf.group(tf.global_variables_initializer(), tf.local_variables_initializer())
@@ -72,30 +89,33 @@ sess.run(init_op)
 saver = tf.train.Saver()
 
 if FLAGS.train_mode:
-    train_writer = tf.summary.FileWriter('log/' + FLAGS.model_dir, sess.graph)
+    train_writer = tf.summary.FileWriter('log/' + FLAGS.model_dir + '_' + CURRENT_DATE_TIME, sess.graph)
+    recall_k = tf.divide(num_correct, num_examples)
     tf.summary.scalar('x_entropy', x_entropy)
+    recall_k_summary = tf.summary.scalar('recall_k', recall_k)
     merged_summary = tf.summary.merge_all()
-    top_k_op = tf.nn.top_k(predictions, k=5)
+    top_k_op = tf.nn.top_k(predictions, k=TOP_K)
     for e in tqdm(range(FLAGS.num_epochs)):
-        summary, _, emb_context_encoder_inputs_r = sess.run([merged_summary, train_op, emb_context_encoder_inputs], feed_dict={context_encoder_inputs: questions,
+        dot_product_r, x_entropy_op_r, summary, _, emb_context_encoder_inputs_r = sess.run([dot_product, x_entropy_op, merged_summary, train_op, emb_context_encoder_inputs], feed_dict={context_encoder_inputs: questions,
                                                                                                                                questions_seq_length_pc: q_seq_length,
                                                                                                                                response_encoder_inputs: answers,
                                                                                                                                answers_seq_length_pc: a_seq_length,
                                                                                                                                labels_input: labels,
                                                                                                                                embeddings_placeholder: embeddings_array})
         if e % 100 == 0:
-            for i in [6, 2, 15]:
-                q_path = 'question_test_{}_tokenized_emb.txt'.format(i)
-                question_v, q_v_seq_length, answers_all, a_seq_length_all, _ = data_utils.get_data(q_path,
-                                                                                                   'all_answers_tokenized_emb.txt',
-                                                                                                   FLAGS.labels)
-                top_k, predictions_r = sess.run([top_k_op, predictions], feed_dict={context_encoder_inputs: question_v,
-                                                                                    questions_seq_length_pc: q_v_seq_length,
-                                                                                    response_encoder_inputs: answers_all,
-                                                                                    answers_seq_length_pc: a_seq_length_all,
+            num_correct_value = 0
+            for (qi, qt) in questions_eval.items():
+                top_k, predictions_r = sess.run([top_k_op, predictions], feed_dict={context_encoder_inputs: qt[0],
+                                                                                    questions_seq_length_pc: qt[1],
+                                                                                    response_encoder_inputs: answers_all[0:num_distractions],
+                                                                                    answers_seq_length_pc: a_seq_length_all[0:num_distractions],
                                                                                     embeddings_placeholder: embeddings_array})
-                correct = i in top_k.indices
-                print('Prediction for question {} is {}correct: {}'.format(i, '' if correct else 'not ', top_k.indices))
+                correct = qi in top_k.indices
+                if correct:
+                    num_correct_value += 1
+                print('Prediction for question {} is {}correct: {}'.format(qi, '' if correct else 'not ', top_k.indices))
+            summary, _ = sess.run([recall_k_summary, recall_k], feed_dict={num_examples: len(questions_eval), num_correct: num_correct_value})
+            print()
         train_writer.add_summary(summary, e)
     saver.save(sess, FLAGS.model_dir)
 else:
